1829189fdcae9fd543acf3444db2c383dd1d4c45
Noahvocat <128837235+Noahvocat@users.noreply.github.com>
2023-09-01 02:59:01 +0200

    Create firesoul.lua

---
+++ firesoul.lua
@@ -0,0 +1,9 @@
+local package = {}
+package.name = "com.flamztech.firesoul"
+package.instdat = {}
+package.instdat.filemaps = {}
+package.instdat.filemaps["YOUR_PASTE_ID"] = "/bin/yourpackage.lua"
+package.pkgType = "com.pastebin"
+package.unicornSpec = "v1.0.0"
+
+return package
