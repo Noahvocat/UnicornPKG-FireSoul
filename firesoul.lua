b926cf2210be2b169e307fd904632ad58b5fd330
Noahvocat <128837235+Noahvocat@users.noreply.github.com>
2023-09-01 03:04:56 +0200

    Update firesoul.lua

--- firesoul.lua
+++ firesoul.lua
@@ -2,7 +2,7 @@ local package = {}
 package.name = "com.flamztech.firesoul"
 package.instdat = {}
 package.instdat.filemaps = {}
-package.instdat.filemaps["YOUR_PASTE_ID"] = "/bin/yourpackage.lua"
+package.instdat.filemaps["13T7zcu6"] = "/etc/unicorn/remotes/FireSoul.txt"
 package.pkgType = "com.pastebin"
 package.unicornSpec = "v1.0.0"
 
